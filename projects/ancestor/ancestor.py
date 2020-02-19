fb64a9b4084f525b1a8c7cc1d75d62e08a78ff6a
Trip4077 <bj2336@email.vccs.edu>
2020-02-18 22:56:46 -0500

    Ancestor Comments Added

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -2,8 +2,10 @@ from ancestor_graph import Graph
 
 
 def earliest_ancestor(ancestors, starting_node):
+    # Generate Graph Of Reverse Family Tree (earliest generations on bottom)
     graph = Graph()
 
+    # Insert and Connect Graph Vertices
     for tupl in ancestors:
         if tupl[0] not in graph.vertices and tupl[1] not in graph.vertices:
             graph.add_vertex( tupl[1] )
@@ -22,11 +24,12 @@ def earliest_ancestor(ancestors, starting_node):
         else:
             graph.add_edge( tupl[1], tupl[0] )
 
+
+    # Use Depth First Search To Move From Starting Node to Earliest Possible Ancestor Tracking the Lineage
     lineage = graph.dft( starting_node )
-    
 
-
-    if len( lineage ) > 2: 
+    # If the Lineage Has Potential For 2 Parents at Earliest Level, Return Lower ID Parent
+    if len( lineage ) > 2:
         earliest_child = lineage[-3]
         parents = graph.get_neighbors( earliest_child )
 
@@ -34,6 +37,7 @@ def earliest_ancestor(ancestors, starting_node):
             if lineage[-1] > lineage[-2]:
                 return lineage[-2]
 
+    # If Starting Node Has a Single Ancestor
     if len( lineage ) > 1: return lineage[-1]
     else: return -1
 
